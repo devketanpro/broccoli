8ba76b219a61b711fdaf92e068a40b6b62805603
devketanpro <devketanpro11@gmail.com>
2023-09-18 18:02:31 +0530

    - Adds environment file  - Install python-dotenv  - updates Pipfile

--- app/constants.py
+++ app/constants.py
@@ -1,6 +1,9 @@
+import os
+
+API_KEY = os.environ.get('API_KEY')
+MODEL = os.environ.get("MODEL")
+
 SEASONS = ["summer", "spring", "fall", "winter"]
-API_KEY = "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"
-MODEL = "gpt-3.5-turbo"
 PROMPT = (
     "Generate a personalized travel itinerary for a trip to {country}. "
     "The traveler is interested in a vacation during {season}. "
@@ -9,4 +12,3 @@ PROMPT = (
     "Provide short and quick response in three lines"
 )
 MESSAGE = {'role': 'user', 'content': '{prompt}'}
-
