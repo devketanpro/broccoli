4b38a76876f2402b96676220bfbfcf2f0c82e375
devketanpro <devketanpro11@gmail.com>
2023-09-20 01:46:50 +0530

    - Adds docstring to main.travel_recommendation API

--- app/main.py
+++ app/main.py
@@ -11,8 +11,22 @@ app = FastAPI()
 
 @app.get("/")
 async def travel_recommendation(country: str, season: str) -> Union[Dict[str, Any], Dict[str, Union[str, Any]]]:
+    """
+    Provides travel recommendations based on the given country and season.
+
+    Args:
+        country (str): The name of the country for which travel recommendations are requested.
+        season (str): The season for which travel recommendations are requested.
+
+    Returns:
+        Union[Dict[str, Any], Dict[str, Union[str, Any]]]: A dictionary containing travel recommendations.
+        The structure of the dictionary depends on the specific implementation of `get_recommendations` function.
+
+    Raises:
+        HTTPException: If the provided season is not valid.
+
+    """
     # Check if seasons are valid
     if season not in SEASONS:
         raise HTTPException(**SEASON_ERROR)
-
     return get_recommendations(country=country, season=season, get_messages_func=get_messages)
